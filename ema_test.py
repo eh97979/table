f71c4f06c7b42dc187dc1e675d62c8a0872be012
eh97979 <sasha101712@gmail.com>
2022-05-05 00:12:24 +0300

    add sell\buy

--- ema_test.py
+++ ema_test.py
@@ -3,17 +3,34 @@ import talib
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 a1 = yf.download('AAPL', '2000-01-01', '2020-04-01')
 a2 = np.array([1, 2, 3])
-
 a3 = a1['Adj Close']
 sma50 = talib.SMA(a1['Adj Close'], 50)
 sma21 = talib.SMA(a1['Adj Close'], 21)
 
 
-plt.plot(sma50, label='SMA50')
-plt.plot(sma21, label='SMA21')
-plt.plot(a3)
-plt.show()
+b = 0
 
+
+for i in sma50:
+
+    b += 1
+    try:
+        if i >= sma21[b-1]:
+            if i <= sma21[b]:
+                print('покупаю', sma21[b:b+1])
+
+        if i <= sma21[b - 1]:
+            if i >= sma21[b]:
+                print('продаю', sma21[b:b+1])
+    except IndexError:
+        break
+
+
+#plt.plot(sma50, label='SMA50')
+#plt.plot(sma21, label='SMA21')
+#plt.plot(a3)
+#plt.show()
 
