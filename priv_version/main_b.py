b67c6062fbac767a2a7abfaa80b0d066a3c0b474
eh97979 <sasha101712@gmail.com>
2022-05-06 16:51:19 +0300

    better version

--- priv_version/main_b.py
+++ priv_version/main_b.py
@@ -13,34 +13,30 @@ aapl_close = yf_aapl['Adj Close']
 sma50 = talib.SMA(aapl_close, 50)
 sma21 = talib.SMA(aapl_close, 21)
 
-buy_signal = []
-sell_signal = []
+
+date_sma = []
+sma_index = []
 
 
 for i in range(len(yf_aapl)):
     if sma50[i] and sma21[i]:
-
         date_index = aapl_date[i].date()
-
         if sma50[i-1] > sma21[i-1] and sma50[i] < sma21[i]:
-            buy_signal.append(sma21[i])
-            sell_signal.append(np.nan)
+            date_sma.append(aapl_date[i].date())
+            sma_index.append(sma50[i])
             print(date_index, 'Buy')
-
         elif sma50[i-1] < sma21[i-1] and sma50[i] > sma21[i]:
-            buy_signal.append(np.nan)
-            sell_signal.append(sma21[i])
+            date_sma.append(aapl_date[i].date())
+            sma_index.append(sma50[i])
             print(date_index, 'Sell')
 
-        else:
-            buy_signal.append(np.nan)
-            sell_signal.append(np.nan)
 
 
-plt.plot(aapl_date, buy_signal, 'go')
-plt.plot(aapl_date, sell_signal, 'ro')
-plt.plot(sma50)
-plt.plot(sma21)
-plt.plot(aapl_close, 'b')
-plt.legend(['Buy', 'Sell', 'SMA50', 'SMA21', 'AAPL'])
-plt.show()
+# plt.plot(date_sma[1::2], sma_index[1::2], 'go')
+# plt.plot(date_sma[0::2], sma_index[0::2], 'ro')
+#
+# plt.plot(sma50)
+# plt.plot(sma21)
+# plt.plot(aapl_close, 'b')
+# plt.legend(['Buy', 'Sell', 'SMA50', 'SMA21', 'AAPL'])
+# plt.show()
