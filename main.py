8dff6f75a1146f17ad3d106943b877baa335b720
eh97979 <sasha101712@gmail.com>
2022-05-06 15:55:46 +0300

    better code and add graph

--- main.py
+++ main.py
@@ -6,32 +6,38 @@ import datetime
 
 
 yf_aapl = yf.download('AAPL', '2000-01-01', '2020-04-01')
+
+aapl_date = yf_aapl.index
 aapl_close = yf_aapl['Adj Close']
+
 sma50 = talib.SMA(aapl_close, 50)
 sma21 = talib.SMA(aapl_close, 21)
-date_buy_sell = []
-value_buy_sell = []
+
+date_index = -1
+date_sma = []
+sma_index = []
 
 
-for i in range(len(sma50)):
-    if i+1 < 5094:
-        if sma21[i] >= sma50[i]:
-            if sma21[i+1] < sma50[i+1]:
-                value_buy_sell.append(sma21[i])
-                date_buy_sell.append(str(sma21[i:i + 1:])[5:15:])
-        elif sma21[i] <= sma50[i]:
-            if sma21[i+1] > sma50[i+1]:
-                value_buy_sell.append(sma21[i])
-                date_buy_sell.append(str(sma21[i:i + 1:])[5:15:])
+for i in range(len(yf_aapl)):
+    if sma50[i] and sma21[i]:
+        if sma50[i-1] > sma21[i-1] and sma50[i] < sma21[i]:
+            date_sma.append(aapl_date[i].date())
+            sma_index.append(sma50[i])
+            date_index += 1
+            print(date_sma[date_index], 'Buy')
+        elif sma50[i-1] < sma21[i-1] and sma50[i] > sma21[i]:
+            date_sma.append(aapl_date[i].date())
+            sma_index.append(sma50[i])
+            date_index += 1
+            print(date_sma[date_index], 'Sell')
 
 
-print(date_buy_sell)
-print(value_buy_sell)
+# plt.plot(date_sma, sma_index, 'b')
+plt.plot(date_sma[1::2], sma_index[1::2], 'go')
+plt.plot(date_sma[0::2], sma_index[0::2], 'ro')
 
-
-# plt.plot(date_buy_sell[1::2], value_buy_sell[1::2], 'ro')
-# plt.plot(date_buy_sell[0::2], value_buy_sell[0::2], 'ro')
-# plt.plot(sma50)
-# plt.plot(sma21)
-# plt.plot(aapl_close)
+plt.plot(sma50)
+plt.plot(sma21)
+plt.plot(aapl_close, 'b')
+plt.legend(['Buy', 'Sell', 'SMA50', 'SMA21', 'AAPL'])
 plt.show()
